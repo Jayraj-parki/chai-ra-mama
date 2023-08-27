67a3e6eda100f5821c8a7c11d1a4b10115a4d6c6
jayraj-parki <jayrajfd@gmail.com>
2023-08-27 15:22:29 +0530

    first commit

---
+++ models/ExampleModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const exampleSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamp: true,
+    })
+
+const Example = mongoose.models.Example || mongoose.model('Example', exampleSchema)
+
+export default  Example;
