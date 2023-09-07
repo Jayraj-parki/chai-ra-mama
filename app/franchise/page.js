bd9c1e68b6fdab1ebcdb50806280752e3b9c1d31
jayraj-parki <jayrajfd@gmail.com>
2023-09-07 20:13:13 +0530

    started implementing franchise page and changed navbar color

--- app/franchise/page.js
+++ app/franchise/page.js
@@ -1,8 +1,15 @@
+import FranchiseBanner from '@/components/franchise_banner/FranchiseBanner'
+import FranchisePartnerProfit from '@/components/franchise_partner_profit/FranchisePartnerProfit'
+import FranchisePartnership from '@/components/franchise_partnership/FranchisePartnership'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>franchise page</div>
+    <>
+      <FranchiseBanner/>
+      <FranchisePartnership/>
+      <FranchisePartnerProfit/>
+    </>
   )
 }
 
