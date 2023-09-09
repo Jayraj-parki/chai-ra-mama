70a35cad673d03e914ca4091740850bf2da6d4b8
jayraj-parki <jayrajfd@gmail.com>
2023-09-09 18:24:09 +0530

    completed franchisePage & added Resposive code for navbar

--- app/franchise/page.js
+++ app/franchise/page.js
@@ -1,4 +1,5 @@
 import FranchiseBanner from '@/components/franchise_banner/FranchiseBanner'
+import FAQ from '@/components/franchise_faq/FAQ'
 import FranchisePartnerProfit from '@/components/franchise_partner_profit/FranchisePartnerProfit'
 import FranchisePartnership from '@/components/franchise_partnership/FranchisePartnership'
 import React from 'react'
@@ -9,6 +10,7 @@ const page = () => {
       <FranchiseBanner/>
       <FranchisePartnership/>
       <FranchisePartnerProfit/>
+      <FAQ/>
     </>
   )
 }
