b8235ae32d62105ea4fe6d582afd8fec70d9213a
jayraj-parki <jayrajfd@gmail.com>
2023-09-23 15:22:09 +0530

    complted feedback page

--- app/feedback/page.js
+++ app/feedback/page.js
@@ -1,8 +1,13 @@
+import FeedbackCards from '@/components/feedback_Cards/FeedbackCards'
+import FeedbackBanner from '@/components/feedback_banner/FeedbackBanner'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>feedback page</div>
+    <>
+      <FeedbackBanner />
+      <FeedbackCards props={["4.5","4","3","1.4","2.6","5"]}/>
+    </>
   )
 }
 export default page
