fc5246726bee0f38bf298739abc2aa5446dd1d3b
jayraj-parki <jayrajfd@gmail.com>
2023-11-03 01:03:59 +0530

    implemented MyOrder component and added product with delevery status. Also added quantiy handler in checkout section

--- components/user-dashboard/cart/Cart.js
+++ components/user-dashboard/cart/Cart.js
@@ -3,12 +3,29 @@ import { useCartUiContext } from '@/app/dashboard/page';
 import React, { useState } from 'react';
 import style from "./cart.module.scss"
 import Image from "next/image"
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 const Cart = () => {
     const { subMenuData } = useCartUiContext()
-    const [cart, setCart] = useState([])
     const onCheckout = () => { return 1 }
-    const onCommentAdd = () => { return 1 }
     const removeCart = (product) => { return 1 }
+    const [productCounts, setProductCounts] = useState({});
+
+    const handleIncrement = (productId) => {
+        setProductCounts(prevCounts => ({
+            ...prevCounts,
+            [productId]: (prevCounts[productId] || 1) + 1
+        }));
+    };
+
+    const handleDecrement = (productId) => {
+        if (productCounts[productId] >1) {
+            setProductCounts(prevCounts => ({
+                ...prevCounts,
+                [productId]: prevCounts[productId] - 1
+            }));
+        }
+    };
     return (
         <div className={style.cart + " container mt-4"}>
             <h3>My Cart</h3>
@@ -31,7 +48,26 @@ const Cart = () => {
                                     <td className='align-middle' >{index + 1}</td>
                                     <td className='align-middle' >{val?.itemName}</td>
                                     <td className='align-middle'> <Image className="rounded " width={250} height={200} objectFit="cover" src={val?.itemImage || "/assets/images/1.png"} alt="..." /></td>
-                                    <td className='align-middle'> {val?.itemPrice}</td>
+                                    <td className='align-middle text-start'>
+                                        <table className='table text-center text-capitalize'>
+                                            <tbody>
+                                                <tr>
+                                                    <td className=''>
+                                                        {/* quantity <input className={style.quantity +" border-1 p-2 bg-light rounded border-0 "} value={productCounts[val._id] || 1} onChange={(e) => setProductCounts(e.target.value)} type='number' /> */}
+                                                        <RemoveIcon className={ style.quantity +' border-1 p-0  rounded mx-2'} onClick={() => handleDecrement(val?._id)}/>
+                                                        <span  className='h6'>{productCounts[val?._id] || 1}</span>
+                                                        <AddIcon className={ style.quantity +' border-1 p-0  rounded mx-2'} onClick={() => handleIncrement(val?._id)}/>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td className=''>
+                                                        Price: {val?.itemPrice * (productCounts[val?._id]||1)} Rs.
+                                                    </td>
+                                                </tr>
+
+                                            </tbody>
+                                        </table>
+                                    </td>
                                     <td className='text-center align-middle'>
                                         <button onClick={() => removeCart({ id: val?._id, name: val?.itemName, price: val?.itemPrice })} className={style.addToCart + " row btn outline-none border-0 col-12 py-2 px-3 px-md-5 fw-bold text-center my-auto mx-auto"} >Remove from Cart</button>
                                     </td>
@@ -47,7 +83,7 @@ const Cart = () => {
             <div className="row col-auto mx-auto mb-5">
                 <button className="btn col-6 col-md-4 col-xl-3 btn-success mt-3 ms-auto" onClick={onCheckout}>Checkout</button>
             </div>
-        </div>
+        </div >
     );
 };
 
